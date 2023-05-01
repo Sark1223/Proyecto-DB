08eebeaba5c88d142377633fc1cfe734bea46a9b
VilladaEdwinMSTR <93624470+VilladaEdwinMSTR@users.noreply.github.com>
2023-04-30 19:14:19 -0700

    clase

---
+++ P1/A.C. Mascotas Vulnerables - DB/DAL/Conexion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.C.Mascotas_Vulnerables___DB.DAL
+{
+    internal class Conexion
+    {
+        //Conexion Santos Karla♥
+        //public string CadenaConexion = @"server = ANVORGUEZA\SQLEXPRESS; Initial Catalog = RESTAURANTE; Integrated Security = true";
+        //SqlConnection conexion;
+
+        //////Conexion Villada Edwin
+        public string CadenaConexion = @"server = DESKTOP-BNMO14B; Initial Catalog = RESTAURANTE; Integrated Security = true";
+        SqlConnection conexion;
+
+        //METODOS DE CONEXION
+        public SqlConnection EstablecerConexion()
+        {
+            this.conexion = new SqlConnection(this.CadenaConexion);
+            return this.conexion;
+        }
+
+        public DataSet EjecutarSentenciaConRetorno(SqlCommand comando)
+        {
+            DataSet DS = new DataSet();
+            SqlDataAdapter adaptador = new SqlDataAdapter();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd = comando;
+                cmd.Connection = EstablecerConexion();
+                adaptador.SelectCommand = cmd;
+                conexion.Open();
+                adaptador.Fill(DS);
+                conexion.Close();
+
+                return DS;
+            }
+            catch
+            {
+                return DS;
+            }
+        }
+
+        //Metodo para AGREGAR, ELIMINAR Y MODIFICAR
+        public void ejecutarComandoSinRetorno(SqlCommand Comandosql)
+        {
+            SqlCommand comando = Comandosql;
+            comando.Connection = this.EstablecerConexion();
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+    }
+}
--- P1/A.C. Mascotas Vulnerables - DB/PL/frmNuevaAportacion.Designer.cs
+++ P1/A.C. Mascotas Vulnerables - DB/PL/frmNuevaAportacion.Designer.cs
@@ -55,6 +55,7 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.btnAgregarRecibo = new ns1.BunifuThinButton2();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
@@ -73,7 +74,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(602, 43);
+            this.panel1.Size = new System.Drawing.Size(672, 43);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
@@ -104,7 +105,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(104, 72);
+            this.label1.Location = new System.Drawing.Point(81, 136);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(121, 20);
             this.label1.TabIndex = 3;
@@ -114,7 +115,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(62, 118);
+            this.label2.Location = new System.Drawing.Point(39, 182);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 20);
             this.label2.TabIndex = 4;
@@ -124,7 +125,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(62, 181);
+            this.label3.Location = new System.Drawing.Point(182, 182);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(129, 20);
             this.label3.TabIndex = 5;
@@ -134,7 +135,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(62, 242);
+            this.label4.Location = new System.Drawing.Point(372, 182);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(132, 20);
             this.label4.TabIndex = 6;
@@ -143,7 +144,7 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(120)))));
-            this.panel2.Location = new System.Drawing.Point(66, 289);
+            this.panel2.Location = new System.Drawing.Point(376, 229);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(115, 2);
             this.panel2.TabIndex = 7;
@@ -151,7 +152,7 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(120)))));
-            this.panel3.Location = new System.Drawing.Point(66, 168);
+            this.panel3.Location = new System.Drawing.Point(43, 232);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(115, 2);
             this.panel3.TabIndex = 8;
@@ -159,7 +160,7 @@
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(120)))));
-            this.panel4.Location = new System.Drawing.Point(66, 229);
+            this.panel4.Location = new System.Drawing.Point(186, 230);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(115, 2);
             this.panel4.TabIndex = 9;
@@ -168,7 +169,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(384, 72);
+            this.label5.Location = new System.Drawing.Point(262, 50);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(131, 20);
             this.label5.TabIndex = 10;
@@ -177,7 +178,7 @@
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(120)))));
-            this.panel5.Location = new System.Drawing.Point(346, 168);
+            this.panel5.Location = new System.Drawing.Point(435, 116);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(159, 2);
             this.panel5.TabIndex = 12;
@@ -186,7 +187,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(342, 118);
+            this.label6.Location = new System.Drawing.Point(431, 66);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(58, 20);
             this.label6.TabIndex = 11;
@@ -195,7 +196,7 @@
             // panel6
             // 
             this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(120)))));
-            this.panel6.Location = new System.Drawing.Point(347, 229);
+            this.panel6.Location = new System.Drawing.Point(54, 365);
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(159, 2);
             this.panel6.TabIndex = 14;
@@ -204,7 +205,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(342, 181);
+            this.label7.Location = new System.Drawing.Point(49, 317);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(77, 20);
             this.label7.TabIndex = 13;
@@ -213,7 +214,7 @@
             // panel7
             // 
             this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(120)))));
-            this.panel7.Location = new System.Drawing.Point(345, 289);
+            this.panel7.Location = new System.Drawing.Point(248, 364);
             this.panel7.Name = "panel7";
             this.panel7.Size = new System.Drawing.Size(159, 2);
             this.panel7.TabIndex = 16;
@@ -222,7 +223,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(341, 242);
+            this.label8.Location = new System.Drawing.Point(244, 317);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(131, 20);
             this.label8.TabIndex = 15;
@@ -233,7 +234,7 @@
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(243)))), ((int)(((byte)(223)))));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(65, 148);
+            this.textBox1.Location = new System.Drawing.Point(42, 212);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(115, 19);
             this.textBox1.TabIndex = 17;
@@ -244,7 +245,7 @@
             this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(243)))), ((int)(((byte)(223)))));
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(66, 210);
+            this.textBox2.Location = new System.Drawing.Point(186, 211);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(115, 19);
             this.textBox2.TabIndex = 18;
@@ -255,7 +256,7 @@
             this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(243)))), ((int)(((byte)(223)))));
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(65, 270);
+            this.textBox3.Location = new System.Drawing.Point(375, 210);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(115, 19);
             this.textBox3.TabIndex = 19;
@@ -266,7 +267,7 @@
             this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(243)))), ((int)(((byte)(223)))));
             this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(346, 148);
+            this.textBox4.Location = new System.Drawing.Point(435, 96);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(160, 19);
             this.textBox4.TabIndex = 20;
@@ -277,7 +278,7 @@
             this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(243)))), ((int)(((byte)(223)))));
             this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(345, 210);
+            this.textBox5.Location = new System.Drawing.Point(52, 346);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(160, 19);
             this.textBox5.TabIndex = 21;
@@ -288,7 +289,7 @@
             this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(243)))), ((int)(((byte)(223)))));
             this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(345, 270);
+            this.textBox6.Location = new System.Drawing.Point(248, 345);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(160, 19);
             this.textBox6.TabIndex = 22;
@@ -313,19 +314,27 @@
             this.btnAgregarRecibo.IdleFillColor = System.Drawing.Color.White;
             this.btnAgregarRecibo.IdleForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(120)))));
             this.btnAgregarRecibo.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(120)))));
-            this.btnAgregarRecibo.Location = new System.Drawing.Point(241, 343);
+            this.btnAgregarRecibo.Location = new System.Drawing.Point(163, 431);
             this.btnAgregarRecibo.Margin = new System.Windows.Forms.Padding(5);
             this.btnAgregarRecibo.Name = "btnAgregarRecibo";
             this.btnAgregarRecibo.Size = new System.Drawing.Size(111, 50);
             this.btnAgregarRecibo.TabIndex = 23;
             this.btnAgregarRecibo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(419, 49);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(186, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
             // frmNuevaAportacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(243)))), ((int)(((byte)(223)))));
-            this.ClientSize = new System.Drawing.Size(602, 425);
+            this.ClientSize = new System.Drawing.Size(672, 588);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.btnAgregarRecibo);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
@@ -388,5 +397,6 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private ns1.BunifuThinButton2 btnAgregarRecibo;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
